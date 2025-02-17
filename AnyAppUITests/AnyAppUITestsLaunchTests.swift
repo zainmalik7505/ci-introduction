bff562e01f6d9bb91ff72376e0eb9d11302755de
zetro-malik <154502262+zetro-nayatel@users.noreply.github.com>
2025-02-17 14:38:27 +0500

    Testing Fastlane and Github Actions

--- AnyAppUITests/AnyAppUITestsLaunchTests.swift
+++ AnyAppUITests/AnyAppUITestsLaunchTests.swift
@@ -20,6 +20,8 @@ class AnyAppUITestsLaunchTests: XCTestCase {
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
+        
+        //Testing Test 
 
         // Insert steps here to perform after app launch but before taking a screenshot,
         // such as logging into a test account or navigating somewhere in the app
